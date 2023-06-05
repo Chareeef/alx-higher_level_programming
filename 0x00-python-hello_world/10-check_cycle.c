273b795f23146972d846837cc4347d8d0f708b08
Chareeef <youssef.charif.h@gmail.com>
2023-06-05 18:44:46 +0100

    check cycle in singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle in a singly linked list using
+ * Floyd's algorithm
+ * @list: pointer to singly linked list's head
+ *
+ * Return: 1 if there is a cycle, 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *hare = list;
+	listint_t *tortoise = list;
+
+	if (!hare || !list->next || !list->next->next)
+		return (0);
+
+	while (hare)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (hare == tortoise)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
