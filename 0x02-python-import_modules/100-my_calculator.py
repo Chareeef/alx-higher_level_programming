57fcfbdbb4b67e83458e242ceb3eb3439785ca59
Chareeef <youssef.charif.h@gmail.com>
2023-06-08 19:15:57 +0100

    Replace match by if and finish the day 0x02

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,15 +11,14 @@ if __name__ == '__main__':
     b = int(argv[3])
     operator = argv[2]
 
-    match operator:
-        case '+':
-            print('{} + {} = {}'.format(a, b, add(a, b)))
-        case '-':
-            print('{} - {} = {}'.format(a, b, sub(a, b)))
-        case '*':
-            print('{} * {} = {}'.format(a, b, mul(a, b)))
-        case '/':
-            print('{} / {} = {}'.format(a, b, div(a, b)))
-        case _:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
+    if operator == '+':
+        print('{} + {} = {}'.format(a, b, add(a, b)))
+    elif operator == '-':
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
+    elif operator == '*':
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
+    elif operator == '/':
+        print('{} / {} = {}'.format(a, b, div(a, b)))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
