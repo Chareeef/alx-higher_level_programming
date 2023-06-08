435c78030aea82c9d646cffaf1ce17f3a3961813
Chareeef <youssef.charif.h@gmail.com>
2023-06-08 11:47:04 +0100

    Add a and b, only if __name__ == '__main__'

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
+import sys
 
-a = 1
-b = 2
-
-print(f'{a} + {b} = {add(a, b)}')
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print(f'{a} + {b} = {add(a, b)}')
