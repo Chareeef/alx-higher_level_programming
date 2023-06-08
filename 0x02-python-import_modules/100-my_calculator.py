034e6edb9e3ce8ebcf4a3f902f9891df9c6da622
Chareeef <youssef.charif.h@gmail.com>
2023-06-08 15:39:52 +0100

    Implement my_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator not in ('+', '-', '*', '/'):
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+    match operator:
+        case '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+        case '-':
+             print('{} - {} = {}'.format(a, b, sub(a, b)))
+        case '*':
+             print('{} * {} = {}'.format(a, b, mul(a, b)))
+        case '/':
+             print('{} / {} = {}'.format(a, b, div(a, b)))
