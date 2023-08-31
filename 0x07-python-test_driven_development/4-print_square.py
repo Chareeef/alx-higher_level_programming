4bb77f464db6316d6491319eb86380017258deb7
Chareeef <youssef.charif.h@gmail.com>
2023-08-31 19:01:12 +0100

    Add 4-print_square.py with its test file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+Implementation of 4-print_square
+Tested with tests/4-print_square.txt
+'''
+
+def print_square(size):
+    '''
+    Prints a square with the character #.
+
+    Args:
+    size (int): the square array's size
+    '''
+
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+
+    if size == 0:
+        print()
+        return
+    
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('tests/4-print_square.txt')
