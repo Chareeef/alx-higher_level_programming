c4d64019ee88ab7c7732356fa24852bb4d4ba1d4
Chareeef <youssef.charif.h@gmail.com>
2023-09-11 17:14:31 +0100

    Add test file for 7-base_geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''This module contains the BaseGeometry class - task 6'''
+'''This module contains the BaseGeometry class - task 7'''
 
 
 class BaseGeometry():
@@ -12,11 +12,13 @@ class BaseGeometry():
 
     def area(self):
         '''Not implemented yet. It raises an Exception'''
+
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         '''Validate a passed value to be an integer greater or equal than 1'''
-        if not isinstance(value, int):
+
+        if type(value) is not int:
             raise TypeError(f'{name} must be an integer')
         if value < 1:
             raise ValueError(f'{name} must be greater than 0')
