efe0b09f849012c95b8c8202ba3647649e81959f
Chareeef <youssef.charif.h@gmail.com>
2023-09-11 12:07:39 +0100

    Add 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This module implements lookup() function for task 0'''
+
+
+def lookup(obj):
+    '''Returns the list of available attributes and methods of an object'''
+
+    return dir(obj)
