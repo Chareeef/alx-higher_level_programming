e9c055de6ad487cf0ab7fa54b60e278f216c5f58
Chareeef <youssef.charif.h@gmail.com>
2023-09-11 14:56:04 +0100

    Add 6-base_geometry.py - area() not implemented yet

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This module contains the BaseGeometry class - task 6'''
+
+
+class BaseGeometry():
+    '''An BaseGeometry class
+
+    Methods:
+        area(self)
+    '''
+
+    def area(self):
+        '''Not implemented yet. It raises an Exception'''
+        raise Exception('area() is not implemented')
