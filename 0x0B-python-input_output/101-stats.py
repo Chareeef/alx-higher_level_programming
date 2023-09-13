6132b7aaefe93530614ce4053075d4172811acee
Chareeef <youssef.charif.h@gmail.com>
2023-09-13 18:32:50 +0100

    Enhance 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,57 @@
 #!/usr/bin/python3
 '''A script that reads stdin line by line and computes metrics'''
 
-import sys
-
 
 if __name__ == '__main__':
-    file_size = 0
-    status = {'200': 0,
-              '301': 0,
-              '400': 0,
-              '401': 0,
-              '403': 0,
-              '404': 0,
-              '405': 0,
-              '500': 0
-              }
+    import sys
 
-    try:
-        while True:
+    def print_metrics(file_size, status_codes):
+        '''Print formatted metrics'''
 
-            for n in range(10):
-                splitted_line = sys.stdin.readline().split()
-                file_size += int(splitted_line[-1])
-                if splitted_line[-2] in status:
-                    status[splitted_line[-2]] += 1
-
-            print('File size:', file_size)
-
-            for k, v in status.items():
-                if v > 0:
-                    print(f'{k}: {v}')
-
-    except KeyboardInterrupt:
         print('File size:', file_size)
 
-        for k, v in status.items():
-            if v > 0:
-                print(f'{k}: {v}')
+        for k in sorted(status_codes):
+            if status_codes[k] > 0:
+                print(f'{k}: {status_codes[k]}')
+
+    dont_stop = True
+    file_size = 0
+    status_codes = {'200': 0,
+                    '301': 0,
+                    '400': 0,
+                    '401': 0,
+                    '403': 0,
+                    '404': 0,
+                    '405': 0,
+                    '500': 0
+                    }
+
+    try:
+        while dont_stop:
+
+            n = 0
+            while n < 10:
+                line = sys.stdin.readline()
+                if line == '':
+                    dont_stop = False
+                    break
+
+                splitted_line = line.split()
+                try:
+                    file_size += int(splitted_line[-1])
+                except (IndexError, ValueError):
+                    pass
+
+                try:
+                    if splitted_line[-2] in status_codes:
+                        status_codes[splitted_line[-2]] += 1
+                except IndexError:
+                    pass
+
+                n += 1
+
+            print_metrics(file_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_metrics(file_size, status_codes)
+        raise
