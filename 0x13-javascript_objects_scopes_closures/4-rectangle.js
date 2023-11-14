2ac02ff97e50b48d708d5787749599672f216181
Chareeef <youssef.charif.h@gmail.com>
2023-11-14 17:41:28 +0100

    Add 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+
+      console.log(row);
+    }
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+module.exports = Rectangle;
