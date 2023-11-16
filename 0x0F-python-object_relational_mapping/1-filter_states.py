da9394a5ba86526f6067679151be2a273e2a72d6
Chareeef <youssef.charif.h@gmail.com>
2023-11-16 09:24:38 -0800

    Add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 '''Script that lists all states with a name starting with N (upper N)'''
-import MySQLdb
-from sys import argv
 
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
 
-conn = MySQLdb.connect(host='localhost', port=3306, user=user,
-        passwd=passwd, db=db)
-curr = conn.cursor()
+if __name__ == '__main__':
 
-curr.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id ASC')
+    import MySQLdb
+    from sys import argv
 
-rows = curr.fetchall()
-for row in rows:
-    print(row)
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=user,
+                                passwd=passwd, db=db)
+    curr = conn.cursor()
+
+    curr.execute('SELECT * FROM states WHERE name LIKE "N%"'
+                 'ORDER BY id ASC')
+
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-'''Script that lists all states from the database hbtn_0e_0_usa'''
+'''
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+'''
 
 
 if __name__ == '__main__':
@@ -10,12 +13,14 @@ if __name__ == '__main__':
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
+    name = argv[4]
 
     conn = MySQLdb.connect(host='localhost', port=3306, user=user,
                                 passwd=passwd, db=db)
     curr = conn.cursor()
 
-    curr.execute('SELECT * FROM states ORDER BY states.id ASC')
+    curr.execute('SELECT * FROM states WHERE name = "{}"'.format(name) +
+                 ' ORDER BY id ASC')
 
     rows = curr.fetchall()
     for row in rows:
