02091b2034566edee9289a16b5516a3964d8fa08
Chareeef <youssef.charif.h@gmail.com>
2024-01-10 21:57:13 +0100

    Say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $.ajax({
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    method: 'GET',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    }
+  });
+});
