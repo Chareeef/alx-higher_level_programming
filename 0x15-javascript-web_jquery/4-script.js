d714a2e42b66fc1ded2a569c19ad6656305526d2
Chareeef <youssef.charif.h@gmail.com>
2024-01-10 18:13:02 +0100

    Toggle header colors

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$( function() {
+  $('div#toggle_header').bind('click', function() {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    } else {
+      if ($('header').hasClass('red')) {
+        $('header').removeClass('red');
+      }
+      $('header').addClass('green');
+    }
+  });
+});
